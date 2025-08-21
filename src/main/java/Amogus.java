c09a2f1437e536a2e8a4f819a6a387e848daf25e
hansonpoh <hansonpoh@gmail.com>
2025-08-21 20:13:07 +0800

    Edit skeletal version of chatbot

---
+++ src/main/java/Amogus.java
@@ -0,0 +1,11 @@
+public class Amogus {
+
+    static final String name = "Amogus";
+
+    public static void main(String[] args) {
+        String horiLines = "____________________________________________________________\n";
+        String intro = "Hello! I'm " + Amogus.name + "!\nWhat can I do for you?\n";
+        String outro = "Bye. Hope to see you again soon!\n";
+        System.out.println(horiLines + intro + horiLines + outro + horiLines);
+    }
+}

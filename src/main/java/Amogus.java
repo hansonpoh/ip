668c2f15c44c9f46cf71245147a8a60e2e1a3bfe
hansonpoh <hansonpoh@gmail.com>
2025-08-21 21:35:47 +0800

    Improve chat fn for further abilities

--- src/main/java/Amogus.java
+++ src/main/java/Amogus.java
@@ -10,18 +10,18 @@ public class Amogus {
     static final String welcome = horiLines + intro + horiLines;
     static final String end = horiLines + outro + horiLines;
 
-    static String[] list = new String[100];
+    static Task[] list = new Task[100];
 
     /**
      * Sends out a welcome message before waiting for the users
-     * input to either add to a list, list the list, or end
-     * the conversation
+     * input for various modes of activity, either listing, marking,
+     * or unmarking tasks.
      */
     public static void Chat() {
         System.out.println(welcome);
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        String input = scanner.next();
         int idx = 0;
         while (true) {
             if (Objects.equals(input, "bye")) {
@@ -34,17 +34,27 @@ public class Amogus {
                     String msg = "";
                     for (int i = 0; i < idx; i++) {
                         int j = i + 1;
-                        msg += j + ". " + list[i] + "\n";
+                        msg += j + "." + list[i].fullDesc() + "\n";
                     }
                     System.out.println(horiLines + msg + horiLines);
                 }
+            } else if (Objects.equals(input, "mark")) {
+                int idxMark = scanner.nextInt();
+                list[idxMark - 1].mark();
+                String msg = horiLines + "Nice! I've marked this task as done:\n" + "  " + list[idxMark - 1].fullDesc() + "\n" + horiLines;
+                System.out.println(msg);
+            } else if (Objects.equals(input, "unmark")) {
+                int idxMark = scanner.nextInt();
+                list[idxMark - 1].unmark();
+                String msg = horiLines + "OK, I've marked this task as not done yet:\n" + "  " + list[idxMark - 1].fullDesc() + "\n" + horiLines;
+                System.out.println(msg);
             } else {
-                list[idx] = input;
+                list[idx] = new Task(input);
                 System.out.println(horiLines + "added: " + input + "\n" + horiLines);
                 idx++;
             }
 
-            input = scanner.nextLine();
+            input = scanner.next();
         }
     }
 
